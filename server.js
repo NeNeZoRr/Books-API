54b7c7c5c91174204b467f89a1f97be25d48d84e
NeNeZoRr <zorrillalin@gmail.com>
2023-11-13 16:07:21 -0500

    Update 5

--- server.js
+++ server.js
@@ -1,30 +1,35 @@
 // DEPENDENCIES
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv'); // Added dotenv for environment variables
+const booksController = require('./controllers/books_controller'); // Corrected import path
 
 // CONFIG/MIDDLEWARE
-require('dotenv').config()
-const PORT = process.env.PORT
-app.use(express.json())
-app.use(cors())
+dotenv.config();
+const PORT = process.env.PORT || 3000; // Added a default value for PORT
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 // MONGOOSE
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
-    console.log('connected to mongo on: ', process.env.MONGO_URI)
-})
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if (err) {
+        console.error('Error connecting to MongoDB:', err);
+    } else {
+        console.log('Connected to MongoDB on:', process.env.MONGO_URI);
+    }
+});
 
-// ROOT INDEX 
+// ROOT INDEX
 app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+    res.send('Hello World');
+});
 
-// BOOKS 
-const booksController = require('./controllers/books_controller')
-app.use('/books', booksController)
+// BOOKS
+app.use('/books', booksController);
 
 // LISTEN
 app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`)
-})
+    console.log(`Listening on port: ${PORT}`);
+});
