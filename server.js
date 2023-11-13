fad38464aafe6c5a6af7096bc159ca92a71db9b0
NeNeZoRr <zorrillalin@gmail.com>
2023-11-13 16:11:38 -0500

    Update7

--- server.js
+++ server.js
@@ -2,24 +2,24 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv'); // Added dotenv for environment variables
-const booksController = require('./controllers/books_controller'); // Corrected import path
+const dotenv = require('dotenv');
+const booksController = require('./controllers/books_controller');
 
 // CONFIG/MIDDLEWARE
 dotenv.config();
-const PORT = process.env.PORT || 3000; // Added a default value for PORT
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 // MONGOOSE
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
-    if (err) {
-        console.error('Error connecting to MongoDB:', err);
-    } else {
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
         console.log('Connected to MongoDB on:', process.env.MONGO_URI);
-    }
-});
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+    });
 
 // ROOT INDEX
 app.get('/', (req, res) => {
